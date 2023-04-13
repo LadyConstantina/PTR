2b8bb67f808b5bfa2b1a8503c622b9c5ad28c15b
Constantina Gilca <constantina.gilca@isa.utm.md>
2023-04-13 19:32:48 +0300

    message brocker is a tcp server

--- Project 3_Message Brocker/messagebrocker/lib/MessageBrocker.ex
+++ Project 3_Message Brocker/messagebrocker/lib/MessageBrocker.ex
@@ -2,13 +2,23 @@ defmodule MessageBrocker do
     use GenServer
     require Logger
 
-  def start_link(url) do
-    GenServer.start_link(__MODULE__,url)
+  def start(_type,_args) do
+    GenServer.start_link(__MODULE__,8000)
+    {:ok, self()}
   end
 
-  def init(url) do
+  def init(port) do
     Logger.info("Message Brocker started")
-    {:ok, nil}
+    Logger.info("Accepting connections on port #{port}")
+    {:ok,listen_socket} = :gen_tcp.listen(port,[:binary,{:packet, 0},{:active,true}])
+    {:ok,socket } = :gen_tcp.accept listen_socket
+    {:ok, %{port: port, socket: socket}}
+  end
+
+  def handle_info({:tcp,_socket,packet}, state) do
+      IO.inspect(packet)
+
+    {:noreply, state}
   end
 
 end
